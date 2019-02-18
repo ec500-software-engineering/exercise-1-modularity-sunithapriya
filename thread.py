1f1d400df96148d1c1c6e5dfa834b7cc7cb2d90d
Sunitha Priyadarshini Sampath <sunithap@bu.edu>
2019-02-18 16:19:53 -0500

    Added thread for processing each input and queue for processing output

---
+++ thread.py
@@ -0,0 +1,37 @@
+from randomgen import *
+from input import *
+from storage import *
+from alert_system import *
+from output import *
+import threading 
+import Queue
+
+
+patientInfo = getPatientInfo()
+patientInfo.encode("ascii","replace")
+
+q = Queue.Queue() 
+
+#Process each thread and add the output into a queue for displaying
+def processThread(atientInfo,sensorData):
+	#print(threading.current_thread())
+	insert(patientInfo, sensorData)
+	alert = alertCheck(sensorData)
+	q.put(alert)
+	
+#Generate 3 random inputs and start a thread for each input
+for i in range(3):
+	sensorData = randomData()
+	t = threading.Thread(target=processThread, args = (patientInfo,sensorData))
+	t.daemon = True
+	t.start()
+
+#Display data from queue into output
+while q.empty():
+	alert = q.get()
+	#Output Module
+	patientOp = patient()
+	#Recieve message from Alert system
+	patientOp.recieveFromAlert(alert)
+	#Display alert to UI
+	patientOp.send_alert_to_UI()
